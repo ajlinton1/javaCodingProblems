5c2fe75ca7c46ffc12a4d749e1d58dabfc97aef0
AndyL <ajlinton@hotmail.com>
2020-10-29 22:09:41 -0700

    LongestCommonPrefix

--- javaCodingProblems1/src/chapter1/LongestCommonPrefix.java
+++ javaCodingProblems1/src/chapter1/LongestCommonPrefix.java
@@ -3,9 +3,29 @@ package chapter1;
 public class LongestCommonPrefix {
 
     public static String getLongestCommonPrefix(String[] s) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
+        int pos = 0;
+        while (true) {
+            if (s.length == 0) {
+                break;
+            }
+            if (s[0].length()==0) {
+                break;
+            }
+            char currentPrefix = s[0].charAt(pos);
+            for (int i=0; i<s.length; i++) {
+                if (s[i].length()==pos) {
+                    return result.toString();
+                }
+                if (s[i].charAt(pos) != currentPrefix) {
+                    return result.toString();
+                }
+            }
+            result.append(currentPrefix);
+            pos++;
+        }
 
-        return result;
+        return result.toString();
     }
 
 }
--- javaCodingProblems1/test/chapter1/TestLongestCommonPrefix.java
+++ javaCodingProblems1/test/chapter1/TestLongestCommonPrefix.java
@@ -7,8 +7,10 @@ public class TestLongestCommonPrefix {
 
     @Test
     public void testLongestCommonPrefix() {
-        String [] s = new String[1];
+//        String [] s = new String[1];
+        String [] s = {"test", "tea", "tester"};
         String result = LongestCommonPrefix.getLongestCommonPrefix(s);
         Assert.assertNotNull(result);
+        Assert.assertEquals("te", result);
     }
 }
