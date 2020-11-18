668d26c4d0a30ab8931261da2f13e5b808e6388a
AndyL <ajlinton@hotmail.com>
2020-11-17 22:30:27 -0800

    MaxMin

---
+++ javaCodingProblems1/src/chapter1/MaxMin.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+public class MaxMin {
+
+    public static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static int min(int x, int y) {
+        if (x < y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+}
---
+++ javaCodingProblems1/test/chapter1/TestMaxMin.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMaxMin {
+
+    @Test
+    public static
+
+}
