823a0102f1e309df0f7e35c52ee7cda4b5e23588
AndyL <ajlinton@hotmail.com>
2021-01-16 21:59:09 -0800

    Index subrange checker

--- javaCodingProblems1/test/chapter2/TestIndex.java
+++ javaCodingProblems1/test/chapter2/TestIndex.java
@@ -8,9 +8,39 @@ public class TestIndex {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIndex() {
-        int bottom = 0;
         int top = 55;
         int v = 56;
         Objects.checkIndex(v, top);
+    }
+
+    @Test
+    public void testIndex1() {
+        int top = 55;
+        int v = 50;
+        Objects.checkIndex(v, top);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSubrange() {
+        int top = 55;
+        int rangeBottom = 5;
+        int rangeTop = 57;
+        Objects.checkFromToIndex(rangeBottom, rangeTop, top);
+    }
+
+    @Test
+    public void testSubrange1() {
+        int top = 55;
+        int rangeBottom = 5;
+        int rangeTop = 50;
+        Objects.checkFromToIndex(rangeBottom, rangeTop, top);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSubrange2() {
+        int top = 55;
+        int rangeBottom = 30;
+        int rangeTop = 20;
+        Objects.checkFromToIndex(rangeBottom, rangeTop, top);
     }
 }
