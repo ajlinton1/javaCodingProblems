605d594a94bfade09fe12c0b2f2a2fe4e863b475
AndyL <ajlinton@hotmail.com>
2021-03-11 21:47:48 -0800

    Bubble sort

--- javaCodingProblems1/test/arrays/TestSortArray99.java
+++ javaCodingProblems1/test/arrays/TestSortArray99.java
@@ -10,4 +10,27 @@ public class TestSortArray99 {
         var a = ArrayService.getRandomArray();
         Assert.assertNotNull(a);
     }
+
+    @Test
+    public void testBubbleSort() {
+        var a = ArrayService.getRandomArray();
+        var swap = false;
+        do {
+            swap = false;
+            for (var i=0;i<a.length-1;i++) {
+                if (a[i]>a[i+1]) {
+                    var temp = a[i+1];
+                    a[i+1] = a[i];
+                    a[i] = temp;
+                    swap = true;
+                }
+            }
+        } while (swap);
+
+        var previous1 = -1;
+        for (var current: a) {
+            Assert.assertTrue(current >= previous1);
+            previous1 = current;
+        }
+    }
 }
