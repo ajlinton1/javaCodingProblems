d148637fb6577760ecc408759cb028198fdd9a47
AndyL <ajlinton@hotmail.com>
2021-04-05 08:45:06 -0700

    Array object search

--- javaCodingProblems1/test/arrays/TestSearchArray100.java
+++ javaCodingProblems1/test/arrays/TestSearchArray100.java
@@ -3,6 +3,8 @@ package arrays;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class TestSearchArray100 {
 
     @Test
@@ -65,4 +67,42 @@ public class TestSearchArray100 {
         Assert.assertEquals(location, -1);
     }
 
+    @Test
+    public void testSearchFunctional() {
+        var a = ArrayService.getRandomArray();
+        a = Sorter.countingSort(a);
+        int i = (int) ((Math.random() * a.length));
+        var target = a[i];
+
+        var found = Arrays.stream(a).anyMatch(e -> e==target);
+
+        Assert.assertTrue(found);
+    }
+
+    @Test
+    public void testSearchFunctionalNotFound() {
+        var a = ArrayService.getRandomArray();
+        a = Sorter.countingSort(a);
+        int i = (int) ((Math.random() * a.length));
+        var target = 200;
+
+        var found = Arrays.stream(a).anyMatch(e -> e==target);
+
+        Assert.assertFalse(found);
+    }
+
+    @Test
+    public void testSearchMelon() {
+        var numbers = ArrayService.getRandomArray();
+        var melons = new Melon[numbers.length];
+        for (var i=0;i<numbers.length;i++) {
+            melons[i] = new Melon(numbers[i]);
+        }
+
+        Arrays.sort(melons, (Melon o1, Melon o2) -> Integer.compare(o1.getWeight(), o2.getWeight()));
+        int i = (int) ((Math.random() * melons.length));
+        var target = melons[i];
+        var result = Arrays.stream(melons).findAny();
+        Assert.assertTrue(result.isPresent());
+    }
 }
