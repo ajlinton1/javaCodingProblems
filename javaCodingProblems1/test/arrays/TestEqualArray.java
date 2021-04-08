c79b0068b2c4f724cc98ca74d29817fd90bbab54
AndyL <ajlinton@hotmail.com>
2021-04-08 07:46:39 -0700

    Array equal

---
+++ javaCodingProblems1/test/arrays/TestEqualArray.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TestEqualArray {
+
+    @Test
+    public void testArrayNotEqual() {
+        var a = ArrayService.getRandomArray();
+        var b = ArrayService.getRandomArray();
+        var equal = Arrays.equals(a, b);
+
+        Assert.assertFalse(equal);
+    }
+
+    @Test
+    public void testArrayEqual() {
+        var a = ArrayService.getRandomArray();
+        var b = a;
+        var equal = Arrays.equals(a, b);
+
+        Assert.assertTrue(equal);
+    }
+
+    @Test
+    public void testArrayEqual1() {
+        var a = ArrayService.getRandomArray();
+        var b = new int[a.length];
+        for (var i=0;i<b.length;i++) {
+            b[i] = a[i];
+        }
+        var equal = Arrays.equals(a, b);
+
+        Assert.assertTrue(equal);
+    }
+
+}
