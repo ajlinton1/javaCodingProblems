b07d4a883790edf5ce39b9b63472c10990251b7f
AndyL <ajlinton@hotmail.com>
2021-05-21 22:37:10 -0700

    Generic merge

--- javaCodingProblems1/test/arrays/TestMap115.java
+++ javaCodingProblems1/test/arrays/TestMap115.java
@@ -26,8 +26,27 @@ public class TestMap115 {
         Assert.assertEquals(6, map3.keySet().size());
     }
 
-    Map<String, Integer> merge(Map<String, Integer> map1, Map<String, Integer> map2) {
-        var ret = new HashMap<String, Integer>();
+    @Test
+    public void testMapMerge1() {
+        var map1 = new HashMap<String, Integer>();
+        map1.put("a", 1);
+        map1.put("b", 2);
+        map1.put("c", 3);
+
+        var map2 = new HashMap<String, Integer>();
+        map2.put("d", 4);
+        map2.put("e", 5);
+        map2.put("b", 6);
+
+        var map3 = this.<String,Integer>merge(map1, map2);
+        Assert.assertNotNull(map3);
+        Assert.assertEquals(5, map3.keySet().size());
+    }
+
+    // https://learning.oreilly.com/library/view/java-generics-and/0596527756/ch01.html
+
+    <T0, T1> Map<T0, T1> merge(Map<T0, T1> map1, Map<T0, T1> map2) {
+        var ret = new HashMap<T0, T1>();
         for (var key:map1.keySet()) {
             ret.put(key, map1.get(key));
         }
