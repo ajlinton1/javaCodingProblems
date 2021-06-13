e9359d99b024ea112ce711dee6580f763087bd00
AndyL <ajlinton@hotmail.com>
2021-06-13 13:49:29 -0700

    Subsitute list

---
+++ javaCodingProblems1/test/arrays/testCollections122.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testCollections122 {
+
+    @Test
+    public void testThreadSafeCollection() {
+        
+    }
+
+
+}
