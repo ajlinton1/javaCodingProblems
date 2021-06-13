310f38352cab413f6ac4c89b3b201fe70e982d71
AndyL <ajlinton@hotmail.com>
2021-06-13 14:12:54 -0700

    Syncronized collection

--- javaCodingProblems1/test/arrays/testCollections122.java
+++ javaCodingProblems1/test/arrays/testCollections122.java
@@ -3,11 +3,24 @@ package arrays;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class testCollections122 {
 
     @Test
     public void testThreadSafeCollection() {
-        
+        Collection<String> collection = new ArrayList<>();
+        collection.add("a");
+        collection.add("b");
+        collection.add("c");
+
+        var collectionSync = Collections.synchronizedCollection(collection);
+        Assert.assertNotNull(collectionSync);
+        for (var s:collectionSync) {
+            Assert.assertNotNull(s);
+        }
     }
 
 
