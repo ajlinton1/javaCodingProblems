6f3170dd596e49e06c2313cb5ef158e3551cf7d9
AndyL <ajlinton@hotmail.com>
2021-06-22 22:38:35 -0700

    Trie implementation

--- javaCodingProblems1/src/trie/Trie.java
+++ javaCodingProblems1/src/trie/Trie.java
@@ -1,5 +1,7 @@
 package trie;
 
+import java.util.function.Function;
+
 public class Trie {
 
     private final Node root;
@@ -9,17 +11,50 @@ public class Trie {
     }
 
     public void insert(String word) {
-
+        var node = root;
+        for (var i=0;i<word.length();i++) {
+            var c = word.charAt(i);
+            Function function = k -> new Node();
+            node = node.getChildren().computeIfAbsent(c, function);
+        }
+        node.setWord(true);
     }
 
     public boolean contains(String word) {
+        Node node = root;
+        for (var i=0;i<word.length();i++) {
+            var ch = word.charAt(i);
+            node = node.getChildren().get(ch);
+            if (node==null) {
+                return false;
+            }
+        }
 
-
-        return false;
+        return node.isWord();
     }
 
     public boolean delete(String word) {
+        return delete(root, word, 0);
+    }
 
+    private boolean delete(Node node, String word, int position) {
+        if (word.length()==position) {
+            if (!node.isWord()) {
+                return false;
+            }
+            node.setWord(false);
+            return node.getChildren().isEmpty();
+        }
+        char ch = word.charAt(position);
+        Node children = node.getChildren().get(ch);
+        if (children==null) {
+            return false;
+        }
+        boolean deleteChildren = delete(children, word, position+1);
+        if (deleteChildren && !children.isWord()) {
+            node.getChildren().remove(ch);
+            return node.getChildren().isEmpty();
+        }
         return false;
     }
 }
