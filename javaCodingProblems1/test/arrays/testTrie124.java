a00a1d593673bbc9ff06b67f8988e0b3863dcfb1
AndyL <ajlinton@hotmail.com>
2021-06-23 21:24:29 -0700

    Trie tests

--- javaCodingProblems1/test/arrays/testTrie124.java
+++ javaCodingProblems1/test/arrays/testTrie124.java
@@ -16,4 +16,21 @@ public class testTrie124 {
         Assert.assertTrue(contains);
     }
 
+    @Test
+    public void testCreateTrie1() {
+        String word = "cat";
+        var trie = new Trie();
+        trie.insert(word);
+        var contains = trie.contains(word);
+        Assert.assertTrue(contains);
+
+        var word1 = "catter";
+        trie.insert(word1);
+        Assert.assertTrue(trie.contains(word1));
+
+        trie.delete(word);
+        Assert.assertFalse(trie.contains(word));
+        Assert.assertTrue(trie.contains(word1));
+    }
+
 }
