ed1425a726bc0f1c5317fbca7e711f23c7cb1d56
ajlinton1 <ajlinton@hotmail.com>
2021-07-30 22:09:19 -0700

    Files walk

---
+++ javaCodingProblems1/test/files/TestFilesWalk.java
@@ -0,0 +1,19 @@
+package files;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestFilesWalk {
+
+    @Test
+    public void testFileWalk() throws Exception {
+        Path startPath = Paths.get("C:\\Python27");
+        var paths = Files.walk(startPath, FileVisitOption.FOLLOW_LINKS);
+        paths.forEach(System.out::println);
+    }
+}
