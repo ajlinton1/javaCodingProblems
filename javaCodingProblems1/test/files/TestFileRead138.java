4be2fdc9f5b3d7ea13aaf1cb19b8766a1a7167a8
ajlinton1 <ajlinton@hotmail.com>
2021-08-05 22:12:54 -0700

    Read file into memory

--- javaCodingProblems1/test/files/TestFileRead138.java
+++ javaCodingProblems1/test/files/TestFileRead138.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 
 import javax.sound.sampled.AudioInputStream;
 import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.EnumSet;
 
@@ -63,6 +66,58 @@ public class TestFileRead138 {
             }
         } catch (Exception ex) {
             System.err.println(ex);
+        }
+    }
+
+    @Test
+    public void testReadFileMemory() throws FileNotFoundException, IOException {
+        var path = Paths.get("C:\\Projects\\javaCodingProblems\\javaCodingProblems1\\src\\files\\CopyFileVisitor.java");
+        var lines = Files.readAllLines(path);
+        lines.forEach(System.out::println);
+    }
+
+    @Test
+    public void testReadFileMemory1() throws FileNotFoundException, IOException {
+        var path = Paths.get("C:\\Projects\\javaCodingProblems\\javaCodingProblems1\\src\\files\\CopyFileVisitor.java");
+        var content = Files.readString(path);
+        Assert.assertNotNull(content);
+    }
+
+    @Test
+    public void testReadFileMemory2() throws FileNotFoundException, IOException {
+        var path = Paths.get("C:\\Projects\\javaCodingProblems\\javaCodingProblems1\\src\\files\\CopyFileVisitor.java");
+        try (FileChannel fileChannel = (FileChannel.open(path, EnumSet.of(StandardOpenOption.READ)))) {
+            MappedByteBuffer mBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            if (mBuffer!=null) {
+                String bufferContent = StandardCharsets.UTF_8.decode(mBuffer).toString();
+                System.out.println(bufferContent);
+                mBuffer.clear();
+            }
+        }
+    }
+
+    private static final int MAP_SIZE = 5242880;
+
+    @Test
+    public void testReadFileMemory3() throws FileNotFoundException, IOException {
+        var path = Paths.get("C:\\Projects\\javaCodingProblems\\javaCodingProblems1\\src\\files\\CopyFileVisitor.java");
+        try (FileChannel fileChannel = (FileChannel.open(path, EnumSet.of(StandardOpenOption.READ)))) {
+
+            int position = 0;
+            long length = fileChannel.size();
+
+            while (position < length) {
+                long remaining = length - position;
+                int bytestomap = (int)Math.min(MAP_SIZE, remaining);
+                MappedByteBuffer mBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, position, bytestomap);
+                if (mBuffer!=null) {
+                    String bufferContent = StandardCharsets.UTF_8.decode(mBuffer).toString();
+                    System.out.println(bufferContent);
+                    mBuffer.clear();
+                }
+
+                position += bytestomap;
+            }
         }
     }
 }
