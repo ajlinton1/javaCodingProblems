8102e468b5b18a25f22bd33d0b5b1700fdd4c30a
ajlinton1 <ajlinton@hotmail.com>
2021-08-10 22:33:44 -0700

    Read binary files

--- javaCodingProblems1/test/files/TestFileRead139.java
+++ javaCodingProblems1/test/files/TestFileRead139.java
@@ -4,13 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.naming.directory.BasicAttribute;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 
 public class TestFileRead139 {
 
@@ -74,6 +75,38 @@ public class TestFileRead139 {
                 System.out.println("Reading");
             }
             System.out.println(new String(buffer));
+        }
+    }
+
+    @Test
+    public void testReadFile4() throws IOException {
+        var path = Paths.get("C:\\Projects\\scala3Test2\\target\\scala-3.0.0\\classes\\hello.class");
+        var bytes = Files.readAllBytes(path);
+        Assert.assertNotNull(bytes);
+    }
+
+    @Test
+    public void testReadFile5() throws IOException {
+        var path = Paths.get("C:\\Projects\\scala3Test2\\target\\scala-3.0.0\\classes\\hello.class");
+        try (FileChannel fileChannel = (FileChannel.open(path, EnumSet.of(StandardOpenOption.READ)))) {
+            MappedByteBuffer mbBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+            System.out.println(mbBuffer.limit());
+        }
+    }
+
+    @Test
+    public void testReadFile6() throws IOException {
+        var path = Paths.get("C:\\Projects\\scala3Test2\\target\\scala-3.0.0\\classes\\hello.class");
+        final int MAP_SIZE = 5242880;
+        try (FileChannel fileChannel = (FileChannel.open(path, EnumSet.of(StandardOpenOption.READ)))) {
+            int position = 0;
+            long length = fileChannel.size();
+            while (position < length) {
+                long remaining = length - position;
+                int bytestomap = (int) Math.min(MAP_SIZE, remaining);
+                MappedByteBuffer mbBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, position, bytestomap);
+                position += bytestomap;
+            }
         }
     }
 
