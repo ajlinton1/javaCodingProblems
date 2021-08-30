66c60272f64258bfa0ac51af3a3b14bcac25f971
ajlinton1 <ajlinton@hotmail.com>
2021-08-29 23:16:43 -0700

    Tokenize file

--- javaCodingProblems1/src/files/FileTokenizer.java
+++ javaCodingProblems1/src/files/FileTokenizer.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileTokenizer {
 
@@ -20,6 +22,45 @@ public class FileTokenizer {
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(delimiterStr);
                 content.addAll(Arrays.asList(values));
+            }
+        }
+        return content;
+    }
+
+    public static List<String> get1(Path path, Charset cs, String delimiter) throws IOException {
+        String delimiterStr = Pattern.quote(delimiter);
+        List<String> content = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(path, cs)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.addAll(Stream.of(line.split(delimiterStr))
+                        .collect(Collectors.toList()));
+            }
+        }
+        return content;
+    }
+
+    public static List<String> get2(Path path, Charset cs, String delimiter) throws IOException {
+        try (Stream<String> lines = Files.lines(path, cs)) {
+            return lines.map(l -> l.split(Pattern.quote(delimiter)))
+                    .flatMap(Arrays::stream)
+                    .collect(Collectors.toList());
+        }
+    }
+
+
+    public static List<String> get4(Path path,
+                                   Charset cs, String delimiter) throws IOException {
+
+        Pattern pattern = Pattern.compile(Pattern.quote(delimiter));
+        List<String> content = new ArrayList<>();
+
+        try (BufferedReader br = Files.newBufferedReader(path, cs)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.add(pattern.splitAsStream(line)
+                        .collect(Collectors.joining(";")));
             }
         }
         return content;
--- javaCodingProblems1/test/files/TestTokenizeFile146.java
+++ javaCodingProblems1/test/files/TestTokenizeFile146.java
@@ -5,14 +5,49 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class TestTokenizeFile146 {
 
     @Test
     public void testTokenizeFile() throws IOException {
-        var path = Paths.get("C:\\Users\\Andy\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
+        var path = Paths.get("C:\\Users\\ajlin\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
         var result = FileTokenizer.get(path, Charset.defaultCharset(), ",");
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testTokenizeFile1() throws IOException {
+        var path = Paths.get("C:\\Users\\ajlin\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
+        var result = FileTokenizer.get1(path, Charset.defaultCharset(), ",");
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testTokenizeFile2() throws IOException {
+        var path = Paths.get("C:\\Users\\ajlin\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
+        var result = FileTokenizer.get2(path, Charset.defaultCharset(), ",");
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testTokenizeFile3() throws IOException {
+        var path = Paths.get("C:\\Users\\ajlin\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
+        var result = Files.readAllLines(path, Charset.defaultCharset()).stream()
+                .map(l -> l.split(Pattern.quote(",")))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testTokenizeFile4() throws IOException {
+        var path = Paths.get("C:\\Users\\ajlin\\OneDrive\\CrunchBase\\20210814\\companies-all-8-14-2021.csv");
+        var result = FileTokenizer.get4(path, Charset.defaultCharset(), ",");
         Assert.assertNotNull(result);
     }
 }
