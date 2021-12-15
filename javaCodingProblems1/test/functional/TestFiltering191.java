72bba145eb617d0bb3b1df9ec473a886f3098d8a
ajlinton1 <ajlinton@hotmail.com>
2021-12-14 21:18:16 -0800

    Mapping

--- javaCodingProblems1/test/functional/TestFiltering191.java
+++ javaCodingProblems1/test/functional/TestFiltering191.java
@@ -3,22 +3,19 @@ package functional;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static java.util.stream.Collectors.*;
 
 public class TestFiltering191 {
 
+    List<Melon> melons = Arrays.asList(new Melon("Crenshaw", 2000),
+            new Melon("Hemi", 1600), new Melon("Gac", 3000),
+            new Melon("Hemi", 2000), new Melon("Crenshaw", 1700),
+            new Melon("Gac", 3000), new Melon("Hemi", 2600));
+
     @Test
     public void testFiltering() {
-        List<Melon> melons = Arrays.asList(new Melon("Crenshaw", 2000),
-                new Melon("Hemi", 1600), new Melon("Gac", 3000),
-                new Melon("Hemi", 2000), new Melon("Crenshaw", 1700),
-                new Melon("Gac", 3000), new Melon("Hemi", 2600));
-
         Map<String, Set<Melon>> melonsFiltering = melons.stream()
                 .collect(groupingBy(Melon::getType,
                         filtering(m -> m.getWeight() > 2000, toSet())));
@@ -42,5 +39,20 @@ public class TestFiltering191 {
                 .collect(partitioningBy(m -> m.getWeight() > 2000, toSet()));
 
         Assert.assertNotNull(melonsFiltering3);
+    }
+
+    @Test
+    public void testMapping() {
+        Map<String, TreeSet<Integer>> melonsMapping = melons.stream()
+                .collect(groupingBy(Melon::getType,
+                        mapping(Melon::getWeight, toCollection(TreeSet::new))));
+
+        Assert.assertNotNull(melonsMapping);
+
+        Map<Boolean, Set<String>> melonsMapping1 = melons.stream()
+                .collect(partitioningBy(m -> m.getWeight() > 2000,
+                        mapping(Melon::getType, toSet())));
+
+        Assert.assertNotNull(melonsMapping1);
     }
 }
