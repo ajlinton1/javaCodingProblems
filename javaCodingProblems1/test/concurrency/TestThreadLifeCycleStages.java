03475cd8b58ae5db80d619af74451443bce6af9b
ajlinton <ajlinton@hotmail.com>
2022-01-19 20:19:39 -0800

    Blocked thread

--- javaCodingProblems1/test/concurrency/TestThreadLifeCycleStages.java
+++ javaCodingProblems1/test/concurrency/TestThreadLifeCycleStages.java
@@ -19,4 +19,41 @@ public class TestThreadLifeCycleStages {
         System.out.println("join");
         Assert.assertEquals(t.getState(), Thread.State.TERMINATED);
     }
+
+    @Test
+    public void testBlocked() throws InterruptedException {
+        Thread t1 = new Thread(new SyncCode());
+        Thread t2 = new Thread(new SyncCode());
+
+        t1.start();
+        Thread.sleep(2000);
+        t2.start();
+        Thread.sleep(2000);
+
+        System.out.println("BlockedThread t1: "
+                + t1.getState() + "(" + t1.getName() + ")");
+        System.out.println("BlockedThread t2: "
+                + t2.getState() + "(" + t2.getName() + ")");
+    }
+
+    private static class SyncCode implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("Thread " + Thread.currentThread().getName()
+                    + " is in run() method");
+            syncMethod();
+        }
+
+        public static synchronized void syncMethod() {
+            System.out.println("Thread " + Thread.currentThread().getName()
+                    + " is in syncMethod() method");
+
+            while (true) {
+                // t1 will stay here forever, therefore t2 is blocked
+            }
+        }
+    }
+
 }
+
