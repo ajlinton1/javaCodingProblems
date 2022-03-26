bee736f3e301b6a1c2078dcf0043814ed0b637d9
ajlinton1 <ajlinton@hotmail.com>
2022-03-26 16:08:16 -0700

    Exchange

---
+++ javaCodingProblems1/test/concurrency/testExchange210.java
@@ -0,0 +1,20 @@
+package concurrency;
+
+import concurrency.exchangers.AssemblyLine;
+import org.junit.Test;
+
+public class testExchange210 {
+
+    // C:\Projects\Java-Coding-Problems\Chapter10\P210_Exchangers\src\modern\challenge\Main.java
+
+    @Test
+    public void testExchange() throws InterruptedException {
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "[%1$tT] [%4$-7s] %5$s %n");
+
+        AssemblyLine.startAssemblyLine();
+        Thread.sleep(60 * 1000);
+        AssemblyLine.stopAssemblyLine();
+
+    }
+}
